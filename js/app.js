e4620c11dd1e4d4f5a8e7d335998d36501457b8e
Florian Tönjes <skatepainter@yahoo.de>
2017-04-12 12:32:13 +0200

    Added displayAlbum function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 "use strict";
 
-$(".desc").hide();
-$(".album-list").hide();
-
-const $album = $(`<li>
-            <div class="album-wrap">
-              <img class="album-art" src="https://i.scdn.co/image/23837f31d4791981db85588e57a86cf2ce5b88e3">
-            </div>
-            <span class="album-title">Luck of the Draw</span>
-            <span class="album-artist">Bonnie Raitt</span>
-          </li>`);
-
+//$(".album-list").hide();
 
 
 $(".search-form").submit(function (evt) {
@@ -21,21 +11,34 @@ $(".search-form").submit(function (evt) {
 });
 
 function displayAlbums(data) {
+    $(".desc").hide();
+    $(".no-albums").remove();
+
     if (data.albums.items.length === 0) {
-        showNoAlbumFound()
+        displayNoAlbumsFound();
     } else {
         $.each(data.albums.items, function (index, album) {
-            console.log(`${index}: ${album.name}`);
+            displayAlbum(album);
         });
     }
 }
 
-function showAlbum() {
+function displayAlbum(album) {
+    console.log(album.artists)
 
+    const $album = $(`<li>
+            <div class="album-wrap">
+              <img class="album-art" src="https://i.scdn.co/image/23837f31d4791981db85588e57a86cf2ce5b88e3">
+            </div>
+            <span class="album-title">${album.name}</span>
+            <span class="album-artist">${album.artists[0].name}</span>
+          </li>`);
+
+    $(".album-list").append($album)
 }
 
-function showNoAlbumFound() {
-    $(".no-albums").remove();
+function displayNoAlbumsFound() {
+
     const query = $("#search").val();
     const $noAlbumFound = $(`<li class='no-albums'>
             <i class='material-icons icon-help'>help_outline</i>No albums found that match: ${query}.
@@ -43,5 +46,3 @@ function showNoAlbumFound() {
 
     $(".main-content").append($noAlbumFound)
 }
-
-//$(".album-list").append($album)
